f629675081e9027a3480d573c35f116fab5ded5b
Xuedong Wang <xuedongwang@tencent.com>
2021-08-06 16:52:38 +0800

    feat(*): 增加 ant-design-vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
+
 import App from './App.vue';
 import store from './store';
 import router from './router';
 import { titleMixin } from './mixins';
+
 import './assets/scss';
+import 'ant-design-vue/dist/antd.css';
 
 Vue.use(titleMixin);
+Vue.use(Antd);
 
 /* eslint-disable no-new */
 new Vue({
